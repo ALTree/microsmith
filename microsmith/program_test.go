e8a2e96ad1870aa748fbe3ebd1b5b2a648757309
Alberto Donizetti <alb.donizetti@gmail.com>
2018-01-20 13:51:01 +0100

    test: skip gc check when -short

--- microsmith/program_test.go
+++ microsmith/program_test.go
@@ -9,35 +9,37 @@ import (
 
 const WorkDir = "../work/"
 
-func TestProgramGeneration(t *testing.T) {
+// check generated programs with go/types (in-memory)
+func TestProgramGotypes(t *testing.T) {
 	rand := rand.New(rand.NewSource(42))
-
-	// check generated programs with go/types (in-memory)
-	t.Run("GoTypesCheck", func(t *testing.T) {
-		for i := 0; i < 2000; i++ {
-			gp := microsmith.NewGoProgram(rand.Int63())
-			err := gp.Check()
-			if err != nil {
-				t.Fatalf("Program failed typechecking: %s\n%s", err, gp)
-			}
+	for i := 0; i < 2000; i++ {
+		gp := microsmith.NewGoProgram(rand.Int63())
+		err := gp.Check()
+		if err != nil {
+			t.Fatalf("Program failed typechecking: %s\n%s", err, gp)
 		}
-	})
+	}
+}
 
-	// check generated programs with gc (from file)
-	t.Run("GcCheck", func(t *testing.T) {
-		for i := 0; i < 10; i++ {
-			gp := microsmith.NewGoProgram(rand.Int63())
-			err := gp.WriteToFile(WorkDir)
-			if err != nil {
-				t.Fatalf("Could not write to file: %s", err)
-			}
-			err = gp.Compile()
-			if err != nil {
-				t.Fatalf("Program did not compile: %s\n%s", err, gp)
-			}
-			gp.DeleteFile()
+// check generated programs with gc (from file)
+// Speed is ~10 program/second
+func TestProgramGc(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	rand := rand.New(rand.NewSource(42))
+	for i := 0; i < 100; i++ {
+		gp := microsmith.NewGoProgram(rand.Int63())
+		err := gp.WriteToFile(WorkDir)
+		if err != nil {
+			t.Fatalf("Could not write to file: %s", err)
 		}
-	})
+		err = gp.Compile()
+		if err != nil {
+			t.Fatalf("Program did not compile: %s\n%s", err, gp)
+		}
+		gp.DeleteFile()
+	}
 }
 
 var gp *microsmith.GoProgram
