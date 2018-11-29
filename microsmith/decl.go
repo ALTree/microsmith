e2af4d40de8a8fc4d6bbd5bc4229dfbbcdf8c46a
Alberto Donizetti <alb.donizetti@gmail.com>
2018-11-29 18:17:11 +0100

    lower chance of literals

--- microsmith/decl.go
+++ microsmith/decl.go
@@ -70,7 +70,7 @@ func RandConf() ProgramConf {
 				float64(rand.Intn(6)), // binary expr
 				float64(rand.Intn(3)), // fun call
 			},
-			LiteralChance:    float64(rand.Intn(9)) * 0.125,
+			LiteralChance:    float64(rand.Intn(5)) * 0.125,
 			ComparisonChance: float64(rand.Intn(9)) * 0.125,
 			IndexChance:      float64(rand.Intn(9)) * 0.125,
 		},
