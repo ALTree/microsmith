4f3821e02fee9cb1c31519980a9cae454acdd958
Alberto Donizetti <alb.donizetti@gmail.com>
2022-09-18 16:49:09 +0200

    clean-up tests

--- microsmith/program_test.go
+++ microsmith/program_test.go
@@ -11,40 +11,7 @@ import (
 
 const WorkDir = "work"
 
-var allTypes = []microsmith.Type{
-	microsmith.BasicType{"bool"},
-	microsmith.BasicType{"byte"},
-	microsmith.BasicType{"int"},
-	microsmith.BasicType{"int8"},
-	microsmith.BasicType{"int16"},
-	microsmith.BasicType{"int32"},
-	microsmith.BasicType{"int64"},
-	microsmith.BasicType{"uint"},
-	microsmith.BasicType{"float32"},
-	microsmith.BasicType{"float64"},
-	microsmith.BasicType{"complex128"},
-	microsmith.BasicType{"rune"},
-	microsmith.BasicType{"string"},
-}
-
-var TestConfigurations = map[string]microsmith.ProgramConf{
-	"small": {
-		MultiPkg: false,
-	},
-
-	"medium": {
-		MultiPkg: false,
-	},
-
-	"big": {
-		MultiPkg: false,
-	},
-	"huge": {
-		MultiPkg: false,
-	},
-}
-
-// check n generated programs with go/types (in-memory)
+// check n generated programs with go/types
 func testProgramGoTypes(t *testing.T, n int, conf microsmith.ProgramConf) {
 	for i := 0; i < n; i++ {
 		gp := microsmith.NewProgram(conf)
@@ -59,68 +26,26 @@ func testProgramGoTypes(t *testing.T, n int, conf microsmith.ProgramConf) {
 	}
 }
 
-func TestRandConf(t *testing.T) {
-	n := 10
+func TestNewProgram(t *testing.T) {
+	n := 50
 	if testing.Short() {
-		n = 5
+		n = 10
 	}
-	for i := 0; i < 10; i++ {
-		// no multipkg, no typeparams
-		testProgramGoTypes(t, n, microsmith.ProgramConf{})
-	}
+	testProgramGoTypes(t, n, microsmith.ProgramConf{})
 }
 
-func TestRandConfTypeParams(t *testing.T) {
-	n := 10
+func TestNewProgramTP(t *testing.T) {
+	n := 50
 	if testing.Short() {
-		n = 5
+		n = 10
 	}
-	for i := 0; i < 10; i++ {
-		testProgramGoTypes(
-			t, n,
-			microsmith.ProgramConf{
-				MultiPkg:   false,
-				TypeParams: true,
-			})
-	}
-}
 
-func TestSmall(t *testing.T) {
-	lim := 100
-	if testing.Short() {
-		lim = 10
-	}
-	testProgramGoTypes(t, lim, TestConfigurations["small"])
-}
-
-func TestMedium(t *testing.T) {
-	lim := 100
-	if testing.Short() {
-		lim = 10
-	}
-	testProgramGoTypes(t, lim, TestConfigurations["medium"])
-}
-
-func TestBig(t *testing.T) {
-	lim := 10
-	if testing.Short() {
-		lim = 2
-	}
-	testProgramGoTypes(t, lim, TestConfigurations["big"])
-}
-
-func TestHuge(t *testing.T) {
-	lim := 5
-	if testing.Short() {
-		lim = 1
-	}
-	testProgramGoTypes(t, lim, TestConfigurations["huge"])
-}
-
-func TestTypeParams(t *testing.T) {
-	conf := TestConfigurations["medium"]
-	conf.TypeParams = true
-	testProgramGoTypes(t, 50, conf)
+	testProgramGoTypes(
+		t, n,
+		microsmith.ProgramConf{
+			MultiPkg:   false,
+			TypeParams: true,
+		})
 }
 
 func GetToolchain() string {
